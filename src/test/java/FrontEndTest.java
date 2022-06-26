4952d6ea952cd691ec425a7a21dd21d2cb25a288
Alex Mink <alexander.minkovsky@gmail.com>
2022-06-26 11:05:22 +0300

    New tests

--- src/test/java/FrontEndTest.java
+++ src/test/java/FrontEndTest.java
@@ -29,6 +29,7 @@ public class FrontEndTest extends BaseTestUtils {
                 By.xpath("//div[@id='fluentform_6_success']")));
         Assert.assertEquals(thankyoutext.getText(), "Дякую, я з вами зв'яжусь");
     }
+
     @Test
     public void sendFormEmptyNameTest() {
         getDriver().get(FRONT_MAIN_URL);
@@ -42,6 +43,7 @@ public class FrontEndTest extends BaseTestUtils {
         WebElement erroremsg = getWait10().until(ExpectedConditions.presenceOfElementLocated(By.className("text-danger")));
         Assert.assertEquals(erroremsg.getText(), "Обов'язково");
     }
+
     @Test
     public void sendFormEmptyEmailTest() {
         getDriver().get(FRONT_MAIN_URL);
@@ -55,6 +57,36 @@ public class FrontEndTest extends BaseTestUtils {
         WebElement erroremsg = getWait10().until(ExpectedConditions.presenceOfElementLocated(By.className("text-danger")));
         Assert.assertEquals(erroremsg.getText(), "Обов'язково");
     }
+
+    @Test
+    public void sendFormEmptyPhoneTest() {
+        getDriver().get(FRONT_MAIN_URL);
+
+        getDriver().findElement(By.name("names[first_name]")).sendKeys("Testname");
+        getDriver().findElement(By.name("email")).sendKeys("test@test.net");
+        getDriver().findElement(By.name("input_mask")).sendKeys("0440000000");
+        getDriver().findElement(By.name("terms-n-condition")).click();
+        getDriver().findElement(By.name("custom_submit_button-6_1")).click();
+
+        WebElement thankyoutext = getWait10().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//div[@id='fluentform_6_success']")));
+        Assert.assertEquals(thankyoutext.getText(), "Дякую, я з вами зв'яжусь");
+    }
+
+    @Test
+    public void sendFormEmptyTermsTest() {
+        getDriver().get(FRONT_MAIN_URL);
+
+        getDriver().findElement(By.name("names[first_name]")).sendKeys("Testname");
+        getDriver().findElement(By.name("email")).sendKeys("test@test.net");
+        getDriver().findElement(By.name("input_mask")).sendKeys("0440000000");
+        //getDriver().findElement(By.name("terms-n-condition")).click();
+        getDriver().findElement(By.name("custom_submit_button-6_1")).click();
+
+        WebElement erroremsg = getWait10().until(ExpectedConditions.presenceOfElementLocated(By.className("text-danger")));
+        Assert.assertEquals(erroremsg.getText(), "Обов'язково");
+    }
+
     @Test
     public void showMenuOnMinResolutionTest() {
         getDriver().get(FRONT_MAIN_URL);
